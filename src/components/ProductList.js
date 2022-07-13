84a6f0ae45399774af66098c38f6a4775343aa83
sanukumar <sanukumar1138@gmail.com>
2022-07-14 01:55:13 +0530

    customize product fields

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Card, Button, Row, Col } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
 import Loader from './Loader';
@@ -10,6 +10,7 @@ import axios from "axios"
 
 const ProductList = ({ products, loading, isUserLoggedIn }) => {
   let history = useHistory();
+  const [customField, setCustomField] = useState({ title: true, manufacture: true, rating: true })
 
   const handleAddProduct = () => {
     if (isUserLoggedIn) {
@@ -72,41 +73,85 @@ const ProductList = ({ products, loading, isUserLoggedIn }) => {
     }
   }
 
+  const onSiteChanged = (e) => {
+    const { name, checked } = e.target
+    setCustomField({
+      ...customField,
+      [name]: checked ? true : false
+    })
+  }
+
   return (
     <>
       <ToastContainer />
-      <div style={{ float: "right", margin: "-5rem 1rem" }}>
-        <Button onClick={handleAddProduct}>Add Product</Button>
-      </div>
-      {loading ? <Loader /> : <Container>
-        <Row className="justify-content-md-center">
-          {products.length ? products.map((p) => {
-            return (
-              <Col xs={12} sm={6} md={6} lg={3} key={p.id}>
-                <Card key={p.id} style={{ margin: '0.2rem' }}>
-                  <Card.Img variant="top" onError={({ currentTarget }) => {
-                    currentTarget.onerror = null; // prevents looping
-                    currentTarget.src = "https://media.istockphoto.com/vectors/no-image-available-sign-vector-id922962354?k=20&m=922962354&s=612x612&w=0&h=f-9tPXlFXtz9vg_-WonCXKCdBuPUevOBkp3DQ-i0xqo=";
-                  }} src={p.thumbnail} height={250} width={250} style={{ padding: "0.3rem" }} onClick={() => handleViewProduct(p.id)} />
-                  <Card.Body>
-                    <Card.Title className='card-text-formatter'>{p.title}</Card.Title>
-                    <Card.Title className='card-text-formatter'>{p.manufacture}</Card.Title>
-                    <Card.Title className='card-text-formatter'><ReactStars edit={false} size={25} value={p.rating} /></Card.Title>
-                    <Card.Text className='card-text-formatter'>
-                      {p.description}
-                    </Card.Text>
-                    <div style={{ display: "flex", justifyContent: "space-between" }}>
-
-                      <Button variant="primary" onClick={() => handleEditProduct(p.id)}>Edit</Button>
-                      <Button variant="danger" onClick={() => handleDeleteProduct(p.id)}>Delete</Button>
-                    </div>
-                  </Card.Body>
-                </Card>
-              </Col>
-            )
-          }) : <h5>No Product Found</h5>}
+      <Container>
+        <Row style={{ marginTop: '-4rem' }}>
+          <Col xs={10} sm={8} >
+            <h2>Customise Product Fields</h2>
+            <form style={{ fontSize: "20px" }}>
+              <label>Title</label>{" "}
+              <input
+                type="checkbox"
+                name="title"
+                checked={customField.title}
+                onChange={onSiteChanged}
+              />{" "}
+              <label>Manufacture</label>{" "}
+              <input
+                type="checkbox"
+                name="manufacture"
+                checked={customField.manufacture}
+                onChange={onSiteChanged}
+              />{" "}
+              <label>Rating</label>{" "}
+              <input
+                type="checkbox"
+                name="rating"
+                checked={customField.rating}
+                onChange={onSiteChanged}
+              />
+            </form>
+          </Col>
+          <Col xs={18} sm={4}>
+            <Button onClick={handleAddProduct} style={{ float: "right" }}>Add Product</Button>
+          </Col>
         </Row>
-      </Container >}
+      </Container>
+
+      <br />
+      {
+        loading ?
+          <Loader /> :
+          <Container>
+            <Row className="justify-content-md-center">
+              {products.length ? products.map((p) => {
+                return (
+                  <Col xs={12} sm={6} md={6} lg={3} key={p.id}>
+                    <Card key={p.id} style={{ margin: '0.2rem' }}>
+                      <Card.Img variant="top" onError={({ currentTarget }) => {
+                        currentTarget.onerror = null; // prevents looping
+                        currentTarget.src = "https://media.istockphoto.com/vectors/no-image-available-sign-vector-id922962354?k=20&m=922962354&s=612x612&w=0&h=f-9tPXlFXtz9vg_-WonCXKCdBuPUevOBkp3DQ-i0xqo=";
+                      }} src={p.thumbnail} height={250} width={250} style={{ padding: "0.3rem" }} onClick={() => handleViewProduct(p.id)} />
+                      <Card.Body>
+                        {customField.title && <Card.Title className='card-text-formatter'>{p.title}</Card.Title>}
+                        {customField.manufacture && <Card.Title className='card-text-formatter'>{p.manufacture}</Card.Title>}
+                        {customField.rating && <Card.Title className='card-text-formatter'><ReactStars edit={false} size={25} value={p.rating} /></Card.Title>}
+                        <Card.Text className='card-text-formatter'>
+                          {p.description}
+                        </Card.Text>
+                        <div style={{ display: "flex", justifyContent: "space-between" }}>
+
+                          <Button variant="primary" onClick={() => handleEditProduct(p.id)}>Edit</Button>
+                          <Button variant="danger" onClick={() => handleDeleteProduct(p.id)}>Delete</Button>
+                        </div>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                )
+              }) : <h5>No Product Found</h5>}
+            </Row>
+          </Container >
+      }
     </>
 
   )
